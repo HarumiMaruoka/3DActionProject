971ee72d8d431fe83ebb720575b43ef64277b30c
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-08-04 11:07:14 +0900

    Revert "Revert "プレイヤーの挙動を作成""
    This reverts commit 8f5cd4dd5e08a3328ac657dd3e63bfc6091ccb59.

---
+++ Assets/MaruokaTestFolder/Player/Script/Weapon/ShootingBase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> ‰“‹——£•Ší‚ÌŠî’êƒNƒ‰ƒX </summary>
+public class ShootingBase : WeaponBase
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MaruokaTestFolder/Player/Script/Weapon/Weapons/BasicSlash.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> Šî–{aŒ‚•Ší </summary>
+public class BasicSlash : SlashingBase
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
